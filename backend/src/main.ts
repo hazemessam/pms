19f4122d1901ad20fef45565a9ff9004fbe0a05d
Hazem Essam <hazemkwita123@gmail.com>
2025-01-21 17:02:12 +0200

    Enable versioning

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
   app.enableCors();
 
   await app.listen(app.get(ConfigService).get('PORT') ?? 3000);
 }
+
 bootstrap();
